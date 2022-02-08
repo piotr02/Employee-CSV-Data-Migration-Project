4d182b69e70be7360da1bd13913ec0c65fec155b
Aiden Sykes <aidensykes1999@gmail.com>
2022-02-08 14:35:02 +0000

    Aiden Sykes - Project Unit Class Tests drafted, TODO Implement

---
+++ src/test/java/CSVShould.java
@@ -0,0 +1,19 @@
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CSVShould {
+
+
+    // Read file Employee.CSV
+    //  add each record read to a new object of a suitable class and then add those objects to a collection
+    // Any corrupt or duplicated data should be added to a separate collection for further analysis.
+    // Consider which date class would be best to use for the date fields – there is one in java.util and another in java.sql.
+    // Provide a simple user interface to display the results of reading the file – how many unique, clean records there are, how many duplicates, how many records with missing fields, possibly display the questionable records.
+    // Since the overall purpose of the project is data migration, we want to make sure that only clean data is transferred.
+
+
+
+}
