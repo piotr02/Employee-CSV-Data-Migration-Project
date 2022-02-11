2fd9a3591ee96ee308951b491d59059a81067826
Piotr Sulek <piotr.sulek@outlook.com>
2022-02-11 20:32:22 +0000

    Improved threading.

--- src/main/java/Model/CSVThreadFactory.java
+++ src/main/java/Model/CSVThreadFactory.java
@@ -7,26 +7,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CSVThreadFactory {
-
     private ArrayList<CSVThread> threadList = new ArrayList<>();
     private ArrayList<String[]> list = new ArrayList<>();
-    private BufferedReader inFile;
 
+    /**
+     * Returns the list populated by threads.
+     * @return list populated by threads
+     */
+    //!!!!!!!!!!
+    // ONLY BECAUSE DATABASE METHODS DON'T WORK NOW
+    //!!!!!!!!!!
     public ArrayList<String[]> getList() {
         return list;
     }
 
-    public void createThreads(int numberOfThreads){
-        try {
-            inFile = new BufferedReader(new FileReader("EmployeeRecordsLarge.csv"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    /**
+     * Creates a list of threads based on desired number of threads.
+     * @param numberOfThreads desired number of threads
+     * @param inFile file to read data from
+     */
+    public void createThreads(int numberOfThreads, BufferedReader inFile){
         for(int i = 0; i < numberOfThreads; i++){
-            this.threadList.add(new CSVThread("thread" + i, list, inFile));
+            this.threadList.add(new CSVThread("thread " + i, list, inFile));
         }
     }
 
+    /**
+     * Starts all threads and then joins them
+     * @param threadList list of threads
+     */
     public void executeThreads(ArrayList<CSVThread> threadList){
         CSVThread[] threads = threadList.toArray(new CSVThread[0]);
         for(int i = 0; i < threads.length; i++){
@@ -41,21 +50,33 @@ public class CSVThreadFactory {
         }
     }
 
+    /**
+     * Runs the threads
+     */
     public void run(){
         executeThreads(threadList);
     }
 
+    //!!!!!!!!!!
+    // TEMPORARY, TO DELETE LATER
+    //!!!!!!!!!!
     public static void main(String[] args) {
         CSVThreadFactory threadFactory = new CSVThreadFactory();
-        threadFactory.createThreads(8);
+        BufferedReader inFile = null;
+        try {
+            inFile = new BufferedReader(new FileReader("EmployeeRecordsLarge.csv"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        threadFactory.createThreads(8, inFile);
         double start = System.currentTimeMillis();
         threadFactory.run();
         double end = System.currentTimeMillis();
 
-        for(String[] record: threadFactory.getList()){
-            System.out.println(Arrays.deepToString(record));
-        }
-        System.out.println(threadFactory.getList().size()-1);
+//        for(String[] record: threadFactory.getList()){
+//            System.out.println(Arrays.deepToString(record));
+//        }
+        System.out.println((threadFactory.getList().size()-1) + " records");
         System.out.println("Process took: " + ((end - start)/1000) + " seconds.");
     }
 }
