4f39d1322c30a43fd633f09f4e462c3d6c9c64cd
AntonyA3 <antonya3@protonmail.com>
2022-02-11 13:50:37 +0000

    Created tests for validators

---
+++ src/test/java/model/validator/IntegerValidatorTest.java
@@ -0,0 +1,21 @@
+package model.validator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class IntegerValidatorTest {
+
+
+    @ParameterizedTest
+    @CsvSource(
+            delimiter = '|',
+            value =
+            {"32|true", "s2|false", "4|true", "1|true", "a|false", "AB|false", "2|true", "3|true"}
+    )
+    void testIntegerValidity(String number, boolean expected){
+        Assertions.assertEquals(expected, new IntegerValidator().validate(number));
+
+    }
+}
