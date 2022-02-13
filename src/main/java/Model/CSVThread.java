ab5963a6c7581007f359c2ed6b71a61ed84a623e
Piotr Sulek <piotr.sulek@outlook.com>
2022-02-13 18:45:42 +0000

    Made changes to the threading

--- src/main/java/Model/CSVThread.java
+++ src/main/java/Model/CSVThread.java
@@ -1,10 +1,16 @@
 package model;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
-public class CSVThread extends Thread{
+public class CSVThread extends Thread {
     private String name;
     private ArrayList<Employee> employeeList;
+//    private Employee employee;
 
     public CSVThread(String name, ArrayList<Employee> employeeList){
         this.name = name;
@@ -15,21 +21,31 @@ public class CSVThread extends Thread{
      * Populates the database with values from the list.
      */
     public void populateDatabase(){
-            for(Employee employee : employeeList) {
-                synchronized (this) {
-                    EmployeeDB.insertEmployee(employee.employer_ID,
-                             employee.prefix,
-                             employee.firstName,
-                             employee.middleInitial,
-                             employee.lastName,
-                             employee.gender,
-                             employee.email,
-                             employee.dateOfBirth,
-                             employee.dateOfJoining,
-                             employee.salary);
-                }
+        for(int i = 0; i < employeeList.size(); i++) {
+            synchronized (employeeList.get(i)) {
+                EmployeeDB.insertEmployee(employeeList.get(i).employer_ID,
+                        employeeList.get(i).prefix,
+                        employeeList.get(i).firstName,
+                        employeeList.get(i).middleInitial,
+                        employeeList.get(i).lastName,
+                        employeeList.get(i).gender,
+                        employeeList.get(i).email,
+                        employeeList.get(i).dateOfBirth,
+                        employeeList.get(i).dateOfJoining,
+                        employeeList.get(i).salary);
             }
-            System.out.println(name + " finished.");
+        }
+//        EmployeeDB.insertEmployee(employee.employer_ID,
+//                employee.prefix,
+//                employee.firstName,
+//                employee.middleInitial,
+//                employee.lastName,
+//                employee.gender,
+//                employee.email,
+//                employee.dateOfBirth,
+//                employee.dateOfJoining,
+//                employee.salary);
+        System.out.println(name + " finished.");
     }
 
     /**
