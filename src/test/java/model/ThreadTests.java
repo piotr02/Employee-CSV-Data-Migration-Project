f3d649a280e43c0bdd72261cc1e7457d400902e5
Piotr Sulek <piotr.sulek@outlook.com>
2022-02-14 00:08:35 +0000

    Added some unit tests

--- src/test/java/model/ThreadTests.java
+++ src/test/java/model/ThreadTests.java
@@ -17,8 +17,7 @@ public class ThreadTests {
 
     @Test
     @DisplayName("Given EmployeeRecords, write these contents to the database using 8 threads")
-    void runTwelveThreadsThroughADatabase(){
-
+    void runEightThreadsThroughADatabase(){
         EmployeeDB employeeDb = new EmployeeDB();
         String[][] csvData = CSVReader.readCsvFile("EmployeeRecords.csv");
         EmployeeCsvDataValidatorNew dataValidator = new EmployeeCsvDataValidatorNew(csvData);
@@ -27,8 +26,61 @@ public class ThreadTests {
 
         createDatabase();
         EmployeeDB.insertEmployeesThreaded(employeeRecords, 8);
+    }
 
+    @Test
+    @DisplayName("Given EmployeeRecords, write these contents to the database using 1 thread")
+    void runOneThreadThroughADatabase(){
+        EmployeeDB employeeDb = new EmployeeDB();
+        String[][] csvData = CSVReader.readCsvFile("TestEmployeeRecords.csv");
+        EmployeeCsvDataValidatorNew dataValidator = new EmployeeCsvDataValidatorNew(csvData);
+        ArrayList<String[]> sqlReadyRecords = new EmployeeDateConversion(dataValidator.getUniqueCleanRecords()).toSqlReadyRecords();
+        ArrayList<Employee> employeeRecords = new RecordsToEmployee(sqlReadyRecords).getEmployeeArrayListFunctional();
 
+        createDatabase();
+        EmployeeDB.insertEmployeesThreaded(employeeRecords, 1);
+    }
+
+    @Test
+    @DisplayName("Given EmployeeRecords, write these contents to the database using 0 threads")
+    void runZeroThreadsThroughADatabase(){
+
+        EmployeeDB employeeDb = new EmployeeDB();
+        String[][] csvData = CSVReader.readCsvFile("EmployeeRecords.csv");
+        EmployeeCsvDataValidatorNew dataValidator = new EmployeeCsvDataValidatorNew(csvData);
+        ArrayList<String[]> sqlReadyRecords = new EmployeeDateConversion(dataValidator.getUniqueCleanRecords()).toSqlReadyRecords();
+        ArrayList<Employee> employeeRecords = new RecordsToEmployee(sqlReadyRecords).getEmployeeArrayListFunctional();
+
+        createDatabase();
+        EmployeeDB.insertEmployeesThreaded(employeeRecords, 0);
+    }
+
+    @Test
+    @DisplayName("Given EmployeeRecords, write these contents to the database using -1 threads")
+    void runMinusOneThreadsThroughADatabase(){
+
+        EmployeeDB employeeDb = new EmployeeDB();
+        String[][] csvData = CSVReader.readCsvFile("EmployeeRecords.csv");
+        EmployeeCsvDataValidatorNew dataValidator = new EmployeeCsvDataValidatorNew(csvData);
+        ArrayList<String[]> sqlReadyRecords = new EmployeeDateConversion(dataValidator.getUniqueCleanRecords()).toSqlReadyRecords();
+        ArrayList<Employee> employeeRecords = new RecordsToEmployee(sqlReadyRecords).getEmployeeArrayListFunctional();
+
+        createDatabase();
+        EmployeeDB.insertEmployeesThreaded(employeeRecords, -1);
+    }
+
+    @Test
+    @DisplayName("Given EmployeeRecords, write these contents to the database using Integer.MAX_VALUE+1 threads")
+    void runMaxIntegerPlusOneThreadsThroughADatabase(){
+
+        EmployeeDB employeeDb = new EmployeeDB();
+        String[][] csvData = CSVReader.readCsvFile("EmployeeRecords.csv");
+        EmployeeCsvDataValidatorNew dataValidator = new EmployeeCsvDataValidatorNew(csvData);
+        ArrayList<String[]> sqlReadyRecords = new EmployeeDateConversion(dataValidator.getUniqueCleanRecords()).toSqlReadyRecords();
+        ArrayList<Employee> employeeRecords = new RecordsToEmployee(sqlReadyRecords).getEmployeeArrayListFunctional();
+
+        createDatabase();
+        EmployeeDB.insertEmployeesThreaded(employeeRecords, Integer.MAX_VALUE+1);
     }
 
 
